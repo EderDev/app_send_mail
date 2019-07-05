65561bd02716b905379c099106d534daeb103b4c
Eder Andrew <contato@andrewdeveloper.com.br>
2019-07-04 22:03:28 -0300

    Finalizando o aplicativo desmonstrativo de envio de email

--- processa_envio.php
+++ processa_envio.php
@@ -12,6 +12,7 @@
         private $para = null;
         private $assunto = null;
         private $mensage = null;
+        public $status = array('codigo_status' => null, 'descricao_status' => '');
 
         public function __get($atributo){
             return $this->$atributo;
@@ -56,7 +57,7 @@
 
         //Recipients
         $mail->setFrom('contato@andrewdeveloper.com.br', 'Andrewdeveloper Remetente');
-        $mail->addAddress('contato@andrewdeveloper.com.br', 'Web Completo Destinatario');     // Add a recipient
+        $mail->addAddress($mensagem->__get('para'));     // Add a recipient
         //$mail->addAddress('ellen@example.com');               // Name is optional
         //$mail->addReplyTo('info@example.com', 'Information');
         //$mail->addCC('cc@example.com');
@@ -68,13 +69,51 @@
 
         //Content
         $mail->isHTML(true);                                  // Set email format to HTML
-        $mail->Subject = 'Assunto do email';
-        $mail->Body    = 'Conteudo do <strong>Email</strong>';
-        $mail->AltBody = 'Eu sou o corpo do Email';
+        $mail->Subject = $mensagem->__get('assunto');
+        $mail->Body    = $mensagem->__get('mensagem');
+        $mail->AltBody = 'É necessário utilizar um client que suporte HTML para ter acesso total ao conteúdo dessa mensagem';
 
         $mail->send();
-        echo 'Mensagem foi enviada';
+        $menagem->status['codigo_status'] = 1;
+        $menagem->status['descricao_status'] = 'E-mail enviado com sucesso';
+        header("Location: index.php?params=msg");
     } catch (Exception $e) {
-        echo 'Mensagem não foi enviada';
-        echo 'Mailer Error: ' . $mail->ErrorInfo;
+        $menagem->status['codigo_status'] = 2;
+        $menagem->status['descricao_status'] = 'Mensagem não foi enviada ' .  $mail->ErrorInfo;
     }
+?>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>App Mail Send</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container">
+            <div class="py-3 text-center">
+				<img class="d-block mx-auto mb-2" src="logo.png" alt="" width="72" height="72">
+				<h2>Send Mail</h2>
+				<p class="lead">Seu app de envio de e-mails particular!</p>
+            </div>
+            
+            <div class="row">
+                <div class="cold-md-2">
+                    <?php if($mensagem->status['codigo_status'] == 1):?>
+                        <div class="container">
+                            <h1 class="display-4 text-success">Sucesso</h1>
+                            <p><?= $mensagem->status['descricao_status']?></p>
+                            <a href="index.php" class="btn btn-success btn-lg mt-5 text-white"></a>
+                        </div>
+                    <?php endif; ?>
+                    <?php if($mensagem->status['codigo_status'] == 2):?>
+                        <div class="container">
+                            <h1 class="display-4 text-success">Ops!</h1>
+                            <p><?= $mensagem->status['descricao_status']?></p>
+                            <a href="index.php" class="btn btn-danger btn-lg mt-5 text-white"></a>
+                        </div>
+                    <?php endif;?>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
